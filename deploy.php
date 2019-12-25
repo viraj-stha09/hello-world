3e7f8dcfd9639884f4ec08ec6fad2ced21ef8b45
Viraj <viraj.shrestha9@gmail.com>
2019-12-25 16:15:16 +0545

    changed the deploy.php

--- deploy.php
+++ deploy.php
@@ -126,7 +126,7 @@ flush();
 $commands = array(
     'echo $PWD',
     'whoami',
-    'git pull',
+    'git pull origin dev',
     'git status',
     'git submodule sync',
     'git submodule update',
